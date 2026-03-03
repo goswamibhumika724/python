2b79978386274ad20ac42fdf9c479cc762642502
goswamibhumika724 <goswamibhumika724@gmail.com>
2026-03-03 12:35:52 +0530

    Add files via upload

---
+++ myself.py
@@ -0,0 +1,17 @@
+# create python program to store below data in variables
+name = "goswami bhumika ashwingiri"
+email = "goswamibhumika724@gmail.com"
+mobile = 9265089850
+age = 20
+weight = 52
+gender = False
+
+# display all variables.
+print(name)
+print(email)
+print(mobile)
+print(age)
+print(weight)
+print(gender)
+
+
