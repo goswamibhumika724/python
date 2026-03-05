0d001153366e7f9a256940aa73280c1313a97b38
goswamibhumika724 <goswamibhumika724@gmail.com>
2026-03-05 14:53:03 +0530

    Add files via upload

---
+++ 7_grams.py
@@ -0,0 +1,15 @@
+#write a program to convert given kilograms into grams & display it 
+kilogram = input("kilogram")
+kilogram = int(kilogram)
+gram = kilogram * 1000
+print("gram =",gram)
+
+
+
+
+
+
+
+
+
+
