ac0fb1579b44cf47d35f32735dbe5e0b7cf60868
goswamibhumika724 <goswamibhumika724@gmail.com>
2026-03-05 14:51:02 +0530

    Add files via upload

---
+++ 4_triangle_area_calculator.py
@@ -0,0 +1,9 @@
+#write a program to calculate area of triangle from given base & height 
+base = input("enter base")
+height = input("enter height")
+
+base = int(base)
+height = int(height)
+
+area = 0.5 * base * height
+print("area of triagle=",area)
