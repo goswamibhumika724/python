8755029849c5093ed7848ce9cef2e5e2649f5f7b
goswamibhumika724 <goswamibhumika724@gmail.com>
2026-03-06 13:00:02 +0530

    Add files via upload

---
+++ 78_tempreture.py
@@ -0,0 +1,6 @@
+#write a program to convert cellcius into ferenhit
+cellcius = input("enter cellcius")
+#convert
+cellcius = float(cellcius)
+ferenhit = (cellcius * 1.8)+32
+print("ferenhit=",ferenhit)
