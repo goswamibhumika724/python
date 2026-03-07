39903979b7d133200115db565bb05683e299bd6a
goswamibhumika724 <goswamibhumika724@gmail.com>
2026-03-07 13:02:22 +0530

    Add files via upload

---
+++ 15_cylinder.py
@@ -0,0 +1,18 @@
+#write a program to findout area & volume of cylinder
+area = input("area")
+volume = input("volume")
+
+#convert
+area = int(area)
+volume = int(volume)
+
+#process
+area = 2 * 3.14 * 6,(78 + 6)
+print("area of cylinder=",area)
+
+volume = 3.14 * 26 * 26 * 47
+volume = round(volume,2)
+print("volume of cylinder=",volume)
+
+
+
