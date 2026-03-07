b9dfe3d8b47d310aa7e904f1d879d5623a3933d0
goswamibhumika724 <goswamibhumika724@gmail.com>
2026-03-07 12:59:57 +0530

    Add files via upload

---
+++ 12_inch.py
@@ -0,0 +1,11 @@
+#write a program to convert meters into inches
+meters = input("meters")
+
+#convert
+meters = int(meters)
+
+#process
+inches = meters * 39.3701
+
+#output
+print("inches=",inches)
