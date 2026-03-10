487e357f02c0de6fb1ad327df531703953ff6fe3
goswamibhumika724 <goswamibhumika724@gmail.com>
2026-03-10 11:18:18 +0530

    Add files via upload

---
+++ 7_string_1.py
@@ -0,0 +1,24 @@
+name = "the easylearn"
+print(name) #the easylearn
+print(name[0]) #print first letter 't'
+print(name[1]) #h print 2nd letter h
+print(name[4]) #e
+
+#print 1st three letter
+print(name[0:3]) #the
+#print all letter from 4th position onwords
+print(name[4:]) #esaylearn
+#print easy
+print(name[4:8])
+#print name variable's value 3 time
+print(name*3)
+city = "bhavnagar"
+print(name + city)
+pincode = 364001
+#print(name + pincode) #error because name is string and pincode is integer
+#city[0] = "B" #error
+city = "Baroda"
+print(city)
+pincode = int(364001)
+print(pincode)
+
