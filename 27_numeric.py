9657031a18bdc92988dd03ef1400505346f1f1a3
goswamibhumika724 <goswamibhumika724@gmail.com>
2026-03-12 13:30:57 +0530

    Add files via upload

---
+++ 27_numeric.py
@@ -0,0 +1,12 @@
+#create numeric list that has 10 number in any order
+number = [39,15,99,80,76,29,55,68,89,91]
+print(number)
+
+#findout minimum value from it
+number.sort()
+print(number)
+
+#findout maximum value from it
+number.reverse()
+print(number)
+
