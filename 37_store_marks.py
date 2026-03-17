967a878395b0f499b4dea5d2e9fc597ba2d8df6b
goswamibhumika724 <goswamibhumika724@gmail.com>
2026-03-17 15:25:19 +0530

    Add files via upload

---
+++ 37_store_marks.py
@@ -0,0 +1,15 @@
+#create dictionary store student name and marks for maths,science,english,computer,drawing
+
+marks = {'name':'bhumika','maths':78,'science':69,'english':92,'computer':75,'drawing':80}
+print(marks)
+
+#sum function is use to get total of all numeric values
+sum = marks['maths'] + marks['science'] + marks['english'] + marks['computer'] + marks['drawing']
+print('total of marks=',sum)
+
+average = sum/5
+print('average',average)
+
+
+
+
