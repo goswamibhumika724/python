d94d6b439ec64f28e30767541227b78f80f42e3d
goswamibhumika724 <goswamibhumika724@gmail.com>
2026-03-25 15:15:57 +0530

    Add files via upload

---
+++ 64_if_elif_else.py
@@ -0,0 +1,36 @@
+#write a program to accept 2 number from user. and accept choice for operations.
+#operations will be addition, subtraction, multiplication, division
+#do operation and display result.
+'''
+# take a any 2 number as input
+# then do it addition,subtraction,multiplication,division
+# display it
+'''
+num1 = int(input('enter num1'))
+num2 = int(input('enter num2'))
+choice = int(input('enter your choice'))
+
+print('press 1 for addition')
+print('press 2 for subtraction')
+print('press 3 for multiplication')
+print('press 4 for division')
+
+
+
+
+
+
+if choice==1:
+    addition = num1 + num2
+    print(f'addition = {addition}')
+elif choice==2:
+    subtraction = num1 - num2
+    print(f'subtraction = {subtraction}')    
+elif choice==3:
+    multiplication = num1 * num2
+    print(f'multiplication = {multiplication}')
+else:
+    division = num1 / num2
+    print(f'division = {division}')
+    
+
