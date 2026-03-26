761a8596df2c1c994832954d041049fdf0cd7e88
goswamibhumika724 <goswamibhumika724@gmail.com>
2026-03-26 13:41:56 +0530

    Add files via upload

---
+++ 68_if_elif.py
@@ -0,0 +1,17 @@
+# write a program to findout whether given year is leap year or not. 
+#take a year as a input
+#findout this year is leap year or not
+year = int(input('enter year'))
+reminder1 = year % 4
+reminder2 = year % 100
+reminder3 = year % 400
+if  reminder1 == 0 and reminder2 != 0:
+    print('this year is leap year')
+else: 
+    if reminder2 == 0 and reminder3 == 0:
+        print('this year is leap year')
+    else:
+        print('this year is not leap year')
+            
+    
+    
