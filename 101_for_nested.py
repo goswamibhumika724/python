00ea4495981a19325bd11801a31a5fa96497d33e
goswamibhumika724 <goswamibhumika724@gmail.com>
2026-04-03 13:51:26 +0530

    Add files via upload

---
+++ 101_for_nested.py
@@ -0,0 +1,17 @@
+#nested loop 
+# write a program to print 30 to 21 multiplication table 
+# 30 x 1 = 30
+# 30 x 2 = 60
+# 30 x 10 = 300
+
+#29 x 1 = 29
+#29 x 10 = 290
+number = 30
+
+while number>=21:
+    multiplier = 1
+    while multiplier<=10:
+        result = number * multiplier
+        print(f'{number} X {multiplier} = {result}')
+        multiplier = multiplier + 1
+    number = number - 1    
