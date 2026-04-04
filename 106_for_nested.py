cc3362ea024c3a6ccef329eb8397e7ec363912d0
goswamibhumika724 <goswamibhumika724@gmail.com>
2026-04-04 14:17:29 +0530

    Add files via upload

---
+++ 106_for_nested.py
@@ -0,0 +1,20 @@
+#* * * * *
+#* * * * *
+#* * * * *
+#* * * * *
+#only 1 astrik per print function 
+#print("*")
+
+for astrik in range(1,5):
+    for astrik in range(1,6):
+        print('*',end=' ')
+    #empty line
+    print(' ')
+    
+# for astrik in range(1,6):
+#     print('*',end=' ')
+# print(' ')
+
+# for astrik in range(1,6):
+#     print('*',end=' ')
+# print(' ')
