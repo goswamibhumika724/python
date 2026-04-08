b53978740d566f3a5603222e29bbd8e84becf83e
goswamibhumika724 <goswamibhumika724@gmail.com>
2026-04-08 15:05:57 +0530

    Add files via upload

---
+++ 119_pip.py
@@ -0,0 +1,10 @@
+#findout python package to convert text to audio (gujarati text gujarati audio )
+from gtts import gTTS
+
+text = "નમસ્તે, તમે કેમ છો?"
+tts = gTTS(text=text, lang='gu')
+
+tts.save("text_audio.mp3")
+print("Audio file created successfully")
+
+ 
