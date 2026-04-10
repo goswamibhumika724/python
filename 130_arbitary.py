28432dbc1c7ac60e4b045fdd4b33c1beec6904dd
goswamibhumika724 <goswamibhumika724@gmail.com>
2026-04-10 14:18:06 +0530

    Add files via upload

---
+++ 130_arbitary.py
@@ -0,0 +1,7 @@
+#write a program to display on that cities whose name end with nagar from all the cities passed as argument
+def nagar_cities(*cities):
+    for city in cities:
+        if "nagar" in city:
+            print(city)
+
+nagar_cities("Ahmedabad", "Jamnagar", "Srinagar", "Surat", "Rajkot", "Gandhinagar")
