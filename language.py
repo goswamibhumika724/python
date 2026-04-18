1b2ea0e09d2679298ce849ae8b73df3c97cc8e26
goswamibhumika724 <goswamibhumika724@gmail.com>
2026-04-18 14:28:24 +0530

    Add files via upload

--- language.py
+++ language.py
@@ -1,19 +1,19 @@
-west_india = [
-    "Gujarati",
-    "Marathi",
-    "Konkani",
-    "Hindi",
-    "Marwari",
-    "Malvi",
-    "Bhili",
-    "Kutchi"
+east_india = [
+    "Bengali",
+    "Odia",
+    "Assamese",
+    "Maithili",
+    "Santali",
+    "Bodo",
+    "Angika",
+    "Chakma"
 ]
 
 def getlanguage():
-    global west_india
-    return west_india
+    global east_india
+    return east_india
 
 def islanguageexist(languagename):
-    global west_india
-    result = languagename in west_india
+    global east_india
+    result = languagename in east_india
     return result
