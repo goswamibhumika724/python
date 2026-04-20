3c24659e26d71823a3f20225eb4564d1d6a7e313
goswamibhumika724 <goswamibhumika724@gmail.com>
2026-04-20 11:14:24 +0530

    Delete 55_cellcius.py
    delete file

--- 55_cellcius.py
+++
@@ -1,8 +0,0 @@
-#write a program to convert & display ferenhit into cellcius
-#input
-ferenhit = input("enter ferenhit")
-#convert
-ferenhit = float(ferenhit)
-cellcius = (ferenhit - 32) /1.8
-cellcius = round(cellcius,2)
-print("cellcius=",cellcius)
