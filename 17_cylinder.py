452460a2332a560fe88a21f536909353e284609e
goswamibhumika724 <goswamibhumika724@gmail.com>
2026-04-20 13:25:50 +0530

    Add files via upload

---
+++ 17_cylinder.py
@@ -0,0 +1,15 @@
+# write a program to findout area and volume of cylinder
+radius = int(input('enter radius'))
+height = int(input('enter height'))
+
+area = 2 * 3.14 * radius * (height + radius)
+volume = 3.14 * radius * radius *  height
+print('area',area)
+print('volume',volume)
+                           
+
+
+
+
+
+
