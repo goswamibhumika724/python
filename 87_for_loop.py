e96ef5acc33e963695dc3e434051589770e8842d
goswamibhumika724 <goswamibhumika724@gmail.com>
2026-04-24 14:24:49 +0530

    Add files via upload

--- 87_for_loop.py
+++ 87_for_loop.py
@@ -6,7 +6,7 @@ numbers = (41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 5
 for item in numbers:
     #print(item,end=' ')
     if item>=50 and item<=100:
-        print('item=',item,end= ' ')
+        print(item,end= ' ')
 
     
     
