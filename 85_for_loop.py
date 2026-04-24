bae4f9569da8e901cd091381749203224960dea2
goswamibhumika724 <goswamibhumika724@gmail.com>
2026-04-24 14:18:20 +0530

    Add files via upload

--- 85_for_loop.py
+++ 85_for_loop.py
@@ -1,7 +1,7 @@
 #display all negative values from list and also count it 
 numbers =numbers = [-1, 2, -3, -4, -5, -6, 7 -8, -9, 10,
            -11, 12, -15, -17, -22, -23, 24, -25]
-count = 1
+count = 0
 for item in numbers:
     #print(item, end= ' ')
     reminder = item // 2 
