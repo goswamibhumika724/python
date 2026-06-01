68cd3179abc2b0c956417cd3d6cf19f1bd3353ed
goswamibhumika724 <goswamibhumika724@gmail.com>
2026-06-01 12:27:40 +0530

    Add files via upload

---
+++ 7_line.py
@@ -0,0 +1,21 @@
+#Create a line chart to display the number of ODI matches played each year in men's cricket from 2000 to 2025.
+import matplotlib.pyplot as plt
+import numpy as np
+
+years = list(range(2000,2026))
+matches =[141, 119, 147, 153, 126, 122, 164, 177, 124, 152, 141, 147, 104, 144, 123, 141, 98, 129, 128, 151, 44, 71, 131, 167, 115, 95]
+
+plt.plot(years,matches)
+plt.title('ODI matches 2000 to 2025')
+plt.xlabel('years',fontsize='11',fontweight='bold',labelpad=10)
+plt.ylabel('matches',fontsize='11',fontweight='bold',labelpad=10)
+
+plt.grid(which='both',linestyle='dotted',linewidth=0.7)
+plt.legend(loc='upper left',title=['ODI matches (india)'])
+plt.xlim(left=1999,right=2026)
+plt.ylim(bottom=44,top=167)
+
+plt.yticks(np.arange(0,max(matches)+26,26))
+plt.xticks(years,rotation=45)
+plt.tight_layout()
+plt.show()
