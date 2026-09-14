1f8c34f963586ee424d4d065672be937b5ba0186
goswamibhumika724 <goswamibhumika724@gmail.com>
2026-09-14 17:05:21 +0530

    Update 151_module.py

--- 151_module.py
+++ 151_module.py
@@ -2,9 +2,9 @@ import currency
 
 rupees = int(input('enter rupees'))
 
-print('dollar',currency.rupeestodollar(rupees))
-print('euro',currency.rupeestoeuro(rupees))
-print('yen',currency.rupeestoyen(rupees))
-print('pound',currency.rupeestopound(rupees))
+print('rupees',currency.rupeestodollar(rupees))
+print('rupees',currency.rupeestoeuro(rupees))
+print('rupees',currency.rupeestoyen(rupees))
+print('rupees',currency.rupeestopound(rupees))
 
 
